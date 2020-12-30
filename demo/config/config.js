73359302bc2a072c375124ac6e053ac7c227259d
zhongzhentao <1158414332@qq.com>
2020-12-30 10:41:51 +0800

    feat(全局): 删除一些需要屏蔽的文件

--- demo/config/config.js
+++ demo/config/config.js
@@ -1,26 +1,29 @@
 import { defineConfig } from 'umi';
-import routes from "./router";
-import globalConfig from "./globalConfig";
+import routes from './router';
+import globalConfig from './globalConfig';
 
 const path = require('path');
+
 const rootdir = path.join(__dirname, '..');
 
 export default defineConfig({
-  nodeModulesTransform: { // node_modules 编译方式
+  nodeModulesTransform: {
+    // node_modules 编译方式
     type: 'none',
   },
   routes,
   hash: true, // 让生成的文件包含 hash 后缀
-  define:{
+  define: {
     globalConfig,
   },
-  devServer:{ 
-    port:12133,
+  devServer: {
+    port: 12133,
   },
-  alias:{
+  alias: {
     '@components': path.resolve(rootdir, 'src/components'),
     '@utils': path.resolve(rootdir, 'src/utils'),
     '@assets': path.resolve(rootdir, 'src/assets'),
     '@services': path.resolve(rootdir, 'src/services'),
   },
+  plugins: [['import', { libraryName: 'antd', style: 'css' }]],
 });

2fb5a369a073c9e16f1ae47eaf4ac9a31b48a500
钟振涛 <zhongzhentao@zhongzhtaodeMBP.111>
2021-03-29 16:54:35 +0800

    修改文件夹名称

---
+++ project/src/pages/demo/index.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Button } from "antd";
+import { Demo } from '@ccf/tools';
+import { goToUrl } from "@ccf/funcs";
+
+export default class index extends Component {
+  changeState = (obj, callback = () => {}) => {
+    this.setState(obj, callback);
+  };
+
+  onClick=()=>{
+    goToUrl('/demo',{a:'122'})
+  }
+
+  render() {
+    return (
+      <div>
+        asdsda
+        <Demo />
+        <Button onClick={this.onClick}>点我</Button>
+      </div>
+    );
+  }
+}
